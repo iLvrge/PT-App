aa4e2fe7b426741096868667030c509dc70556ad
Uzi Aloush <uzi@synpat.com>
2021-03-26 15:20:23 -0700

    Production error

--- src/components/common/AssetsCommentsTimeline/index.js
+++ src/components/common/AssetsCommentsTimeline/index.js
@@ -580,28 +580,19 @@ const AssetsCommentsTimeline = ({ toggleMinimize, size, setChannel, channel_id }
           ?
           <div className={classes.button}>
             {
-              slackAuthLogin 
-              ? 
-                <Button 
-                  onClick={() => {
-                    onHandleSlackLogin(900, 830)
-                  }
-                  }
-                >
-                  <img 
-                    alt='Sign in with Slack' 
-                    height='40' 
-                    width='172' 
-                    src='https://platform.slack-edge.com/img/sign_in_with_slack.png' 
-                    srcSet='https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x' />
-                </Button>
-              :
-                {/* <Button 
-                  onClick={onHandleSlackSignout} 
-                  className={classes.signout}
-                >
-                  <svg width="18" height="18" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><path d="M19.712.133a5.381 5.381 0 0 0-5.376 5.387 5.381 5.381 0 0 0 5.376 5.386h5.376V5.52A5.381 5.381 0 0 0 19.712.133m0 14.365H5.376A5.381 5.381 0 0 0 0 19.884a5.381 5.381 0 0 0 5.376 5.387h14.336a5.381 5.381 0 0 0 5.376-5.387 5.381 5.381 0 0 0-5.376-5.386" fill="#36C5F0"></path><path d="M53.76 19.884a5.381 5.381 0 0 0-5.376-5.386 5.381 5.381 0 0 0-5.376 5.386v5.387h5.376a5.381 5.381 0 0 0 5.376-5.387m-14.336 0V5.52A5.381 5.381 0 0 0 34.048.133a5.381 5.381 0 0 0-5.376 5.387v14.364a5.381 5.381 0 0 0 5.376 5.387 5.381 5.381 0 0 0 5.376-5.387" fill="#2EB67D"></path><path d="M34.048 54a5.381 5.381 0 0 0 5.376-5.387 5.381 5.381 0 0 0-5.376-5.386h-5.376v5.386A5.381 5.381 0 0 0 34.048 54m0-14.365h14.336a5.381 5.381 0 0 0 5.376-5.386 5.381 5.381 0 0 0-5.376-5.387H34.048a5.381 5.381 0 0 0-5.376 5.387 5.381 5.381 0 0 0 5.376 5.386" fill="#ECB22E"></path><path d="M0 34.249a5.381 5.381 0 0 0 5.376 5.386 5.381 5.381 0 0 0 5.376-5.386v-5.387H5.376A5.381 5.381 0 0 0 0 34.25m14.336-.001v14.364A5.381 5.381 0 0 0 19.712 54a5.381 5.381 0 0 0 5.376-5.387V34.25a5.381 5.381 0 0 0-5.376-5.387 5.381 5.381 0 0 0-5.376 5.387" fill="#E01E5A"></path></g></svg> Sign Out
-                </Button> */}
+              slackAuthLogin && (<Button 
+                onClick={() => {
+                  onHandleSlackLogin(900, 830)
+                }
+                }
+              >
+                <img 
+                  alt='Sign in with Slack' 
+                  height='40' 
+                  width='172' 
+                  src='https://platform.slack-edge.com/img/sign_in_with_slack.png' 
+                  srcSet='https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x' />
+              </Button>)              
             }
 
             {
