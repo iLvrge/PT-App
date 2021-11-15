71271f072217b2f513054bce3206943680708449
Uzi Aloush <uzi@synpat.com>
2021-11-15 10:34:05 -0800

    Added fixed bottom navigation

--- src/components/MobileFooter/styles.js
+++ src/components/MobileFooter/styles.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles'
 export default makeStyles(theme => ({
     root: {
         position: 'fixed', 
-        bottom: 4, 
+        bottom: 0, 
         left: 0, 
         right: 0,
         zIndex: 999,
@@ -20,7 +20,8 @@ export default makeStyles(theme => ({
             height: 45
         },
         '& .MuiFab-root':{
-            width: 48
+            width: 48,
+            bottom: 2
         }
     }, 
     alignRight: {
