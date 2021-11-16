cd4ca3e70f403480406f10181cc902c558905f66
Uzi Aloush <uzi@synpat.com>
2021-11-15 22:44:33 -0800

    Fotter position absolute and bottom

--- src/components/MobileFooter/styles.js
+++ src/components/MobileFooter/styles.js
@@ -5,11 +5,11 @@ export default makeStyles(theme => ({
     root: {
         display: 'flex',
         height: 45,
-        width: 'initial',
+        /* width: 'initial',
         position: 'absolute',
         left: 0,
         right: 0,
-        bottom: 0,
+        bottom: 0, */
         zIndex: 999,
         '& svg.MuiSvgIcon-root': {
             width: '2rem',
