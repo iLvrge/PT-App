f3a427fcc95fa5c8bc5ef81565d0796b7ae74f8f
Uzi Aloush <uzi@synpat.com>
2022-03-08 15:38:13 -0800

    Design changes

--- src/components/common/VirtualizedTable/index.js
+++ src/components/common/VirtualizedTable/index.js
@@ -582,7 +582,7 @@ const VirtualizedTable = ({
             ref={rowRef}
             style={{
               marginRight: "auto",
-              marginLeft: typeof childInModal === 'undefined' ? 45 : 0,
+              marginLeft: typeof childInModal === 'undefined' ? 25 : 0,
               height:
                 disableRow === true
                   ? rowData[disableRowKey] * rowHeight < childHeight
