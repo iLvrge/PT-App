30b600902a72665a5f2584d35ddf32970156ec15
Vivek Kapoor <er.vivek2512@gmail.com>
2022-08-04 18:05:25 +0530

    Maintaince merge with Asset Table

--- src/components/common/Maintainence/styles.js
+++ src/components/common/Maintainence/styles.js
@@ -3,6 +3,7 @@ import makeStyles from '@mui/styles/makeStyles';
 export default makeStyles((theme) => ({
     floatIcon: {
         position: 'absolute',
-        right: 0
+        right: 0,
+        zIndex: 9999
     }
 }))
