a088b14ce5d220c389a65c3693dd700f168b136a
Vivek Kapoor <er.vivek2512@gmail.com>
2023-02-19 08:42:49 +0530

    Added virtual table console

--- src/reducers/store/configureStore.js
+++ src/reducers/store/configureStore.js
@@ -13,8 +13,8 @@ import { setTokenStorage, getTokenStorage } from '../../utils/tokenStorage'
 
 import AuthApi from '../../api/authApi'
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger )) 
-/* const store = createStore(rootReducer, applyMiddleware(thunk)) */
+/*const store = createStore(rootReducer, applyMiddleware(thunk, logger )) */
+const store = createStore(rootReducer, applyMiddleware(thunk)) 
 
 if( process.env.REACT_APP_ENVIROMENT_MODE === 'STANDARD' || process.env.REACT_APP_ENVIROMENT_MODE === 'SAMPLE'  || process.env.REACT_APP_ENVIROMENT_MODE === 'DASHBOARD' || process.env.REACT_APP_ENVIROMENT_MODE === 'KPI') {
   let location = window.location.pathname
