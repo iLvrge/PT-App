db959c8039002e008ef495c586b1bd606436af6b
Vivek Kapoor <er.vivek2512@gmail.com>
2023-04-19 04:09:35 +0530

    Header items positions

--- src/components/NewHeader/styles.js
+++ src/components/NewHeader/styles.js
@@ -41,6 +41,7 @@ export default makeStyles(theme => ({
         overflow: 'hidden',
         textOverflow: 'ellipsis',
         fontSize: '1.1rem',
+        marginRight: 'auto',
         '& span':{
             textTransform:'uppercase',
             marginLeft: 24
@@ -128,10 +129,10 @@ export default makeStyles(theme => ({
         whiteSpace: 'nowrap',
         textOverflow: 'ellipsis',
         overflow: 'hidden',
-        width: 500,
+        minWidth: 595,
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'flex-end'
         /* textAlign: 'right' */
     },
     toolbar: {
@@ -163,6 +164,9 @@ export default makeStyles(theme => ({
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        maxWidth: 465,
         '& .MuiBadge-anchorOriginTopRightRectangular': {
             transform: 'scale(1) translate(27%, 0%)',
             padding: '0 3px',
@@ -553,6 +557,8 @@ export default makeStyles(theme => ({
         } */     
         cursor: 'default' ,
         fontSize: '1.1rem',
+        fontWeight: 200,
+        textTransform: 'initial',
         color: `${theme.palette.text.primary}`
     },
     actionMenuList: {
