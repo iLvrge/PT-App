fa79a3372c0ecf14723701b48f58e0e8752bafd9
Vivek Kapoor <er.vivek2512@gmail.com>
2023-10-04 20:59:42 +0530

    Fixed Config in staging

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 /* export const base_api_url = 'http://localhost:1800';
 export const base_new_api_url = 'http://localhost:1800';  */
- export const base_api_url = 'https://betapp.patentrack.com' 
-export const base_new_api_url = 'https://betapp.patentrack.com' 
+ export const base_api_url = 'https://api.patentrack.com' 
+export const base_new_api_url = 'https://api.patentrack.com' 
