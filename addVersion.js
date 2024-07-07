0719a4f6e0b540c49dd3bced8c59e7b0408fd7e3
Vivek Kapoor <er.vivek2512@gmail.com>
2024-07-07 21:44:20 +0530

    Fixed version file code

--- addVersion.js
+++ addVersion.js
@@ -4,6 +4,39 @@ const path = require('path');
 const buildDir = path.join(__dirname, 'build', 'static', 'js');
 const version = process.argv[2] || '1.0.0';
 
+// Function to rename files and update index.html
+const addVersionToFiles = (files) => {
+  files.forEach(file => {
+    const oldPath = path.join(buildDir, file);
+    const newPath = `${oldPath}?v=${version}`;
+
+    console.log(`Renamed ${file} to ${file}?v=${version}`);
+  });
+
+  // Update index.html to reference the new filenames
+  const indexPath = path.join(__dirname, 'build', 'index.html');
+  fs.readFile(indexPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading index.html:', err);
+      return;
+    }
+
+    let updatedData = data;
+    files.forEach(file => {
+      const regex = new RegExp(file, 'g');
+      updatedData = updatedData.replace(regex, `${file}?v=${version}`);
+    });
+
+    fs.writeFile(indexPath, updatedData, 'utf8', err => {
+      if (err) {
+        console.error('Error updating index.html:', err);
+        return;
+      }
+      console.log('Updated index.html with versioned filenames');
+    });
+  });
+};
+
 // Read all files in the build/static/js directory
 fs.readdir(buildDir, (err, files) => {
   if (err) {
@@ -11,39 +44,5 @@ fs.readdir(buildDir, (err, files) => {
     return;
   }
 
-  files.forEach(file => {
-    const oldPath = path.join(buildDir, file);
-    const newPath = path.join(buildDir, `${file}?v=${version}`);
-
-    fs.rename(oldPath, newPath, err => {
-      if (err) {
-        console.error('Error renaming file:', err);
-        return;
-      }
-      console.log(`Renamed ${file} to ${file}?v=${version}`);
-    });
-  });
-});
-
-// Update index.html to reference the new filenames
-const indexPath = path.join(__dirname, 'build', 'index.html');
-fs.readFile(indexPath, 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading index.html:', err);
-    return;
-  }
-
-  let updatedData = data;
-  files.forEach(file => {
-    const regex = new RegExp(file, 'g');
-    updatedData = updatedData.replace(regex, `${file}?v=${version}`);
-  });
-
-  fs.writeFile(indexPath, updatedData, 'utf8', err => {
-    if (err) {
-      console.error('Error updating index.html:', err);
-      return;
-    }
-    console.log('Updated index.html with versioned filenames');
-  });
+  addVersionToFiles(files);
 });
