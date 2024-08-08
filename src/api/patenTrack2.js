f225271307008df5a5d7ebcbeee7cf0734eab300
Vivek Kapoor <er.vivek2512@gmail.com>
2024-08-08 08:43:28 +0530

    Added changes for token null

--- src/api/patenTrack2.js
+++ src/api/patenTrack2.js
@@ -3,34 +3,12 @@ import _toLower from 'lodash/toLower'
 
 import { base_api_url, base_new_api_url } from '../config/config'
 import { convertAssetTypeToTabId } from '../utils/assetTypes'
+import getToken from './token'
 
 export const DEFAULT_CUSTOMERS_LIMIT = 1000
 export const DEFAULT_TRANSACTIONS_LIMIT = 15
 export const DEFAULT_PATENTS_LIMIT = 15
 
-const getCookie = name => {
-  var nameEQ = name + '='
-  var ca = document.cookie.split(';')
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i]
-    while (c.charAt(0) === ' ') c = c.substring(1, c.length)
-    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length)
-  }
-  return null
-}
-
-const getToken = () => {
-  let token = null
-  if( process.env.REACT_APP_ENVIROMENT_MODE === 'STANDARD' || process.env.REACT_APP_ENVIROMENT_MODE === 'SAMPLE'  || process.env.REACT_APP_ENVIROMENT_MODE === 'DASHBOARD' || process.env.REACT_APP_ENVIROMENT_MODE === 'KPI') {
-    token = localStorage.getItem('auth_signature')
-  } else {
-    token = localStorage.getItem('token')
-    if (token === null) {
-      token = getCookie('token')
-    }
-  }
-  return token
-}
 
 const getHeader = () => {
   return {
