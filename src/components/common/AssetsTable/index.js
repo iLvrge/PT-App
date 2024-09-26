b1e9be78ccb37fe3d4eb488a55095415c141a2f8
Vivek Kapoor <er.vivek2512@gmail.com>
2024-09-26 06:22:09 +0530

    From Virtualized dropdown select clipboard and remove item from clipboard then click on no action.

--- src/components/common/AssetsTable/index.js
+++ src/components/common/AssetsTable/index.js
@@ -699,7 +699,7 @@ s4,1.7944336,4,4v4c0,0.5522461,0.4472656,1,1,1H50.2363281z" ></path><path d="M23
   }
  
 
-  const onHandleDropDownlist = async(event, asset, row ) => {  
+  const onHandleDropDownlist = async(event, asset, row ) => {   
     if( process.env.REACT_APP_ENVIROMENT_MODE === 'SAMPLE' ) {
       alert('Message....')
     } else {
@@ -799,7 +799,7 @@ s4,1.7944336,4,4v4c0,0.5522461,0.4472656,1,1,1H50.2363281z" ></path><path d="M23
               */
               alert("Please login to slack first");
             }
-          } else if (event.target.value === 0) {
+          } else if (event.target.value === 0 || event.target.value === 99) {
             setSelectedAssets(prevItems => {
               const findIndex = prevItems.findIndex( r => r.asset == asset)
               if( findIndex !== -1 ) {
