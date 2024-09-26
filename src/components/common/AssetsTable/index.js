a27bfd0b759d15e604eebf1bc02adfd1cb11c167
Vivek Kapoor <er.vivek2512@gmail.com>
2024-09-26 07:16:52 +0530

    Change No Action to Remove from Clipboard

--- src/components/common/AssetsTable/index.js
+++ src/components/common/AssetsTable/index.js
@@ -363,7 +363,7 @@ s4,1.7944336,4,4v4c0,0.5522461,0.4472656,1,1,1H50.2363281z" ></path><path d="M23
   const actionList = [
     {
       id: 99,
-      name: 'No action' ,
+      name: 'Remove from Clipboard' ,
       icon: <NotInterested />,
       image: '',
       tooltip: 'Tooltip'
